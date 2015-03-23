73f9b7455c0177097213ead2c796577b5771791e
wisdommm <13107724281@163.com>
2015-03-23 19:00:01 +0800

    Signed-off-by: wisdommm <13107724281@163.com>

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+	$('.link .button').hover(function(){
+		var title=$(this).attr('data-title');
+		document.title=title;
+		$('.tip em').text(title);
+		var pos=$(this).offset().left;
+		var dis=($('.tip').outerWidth()-$(this).outerWidth())/2;
+		var l=pos-dis;
+		$('.tip').css({'left':l+'px'}).animate({'top':180,'opacity':1},300);
+	},function(){
+		$('.tip').animate({'top':160,'opacity':0},300);
+	})
+})
